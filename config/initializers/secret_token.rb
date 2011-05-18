7980fd1b41e1538cf747def168d12e8237a02482
Dmitry Ratnikov <ratnikov@gmail.com>
2011-05-18 11:54:17 -0400

    Initial generate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppengineRails3::Application.config.secret_token = '1efba133ac4824e9b0b7c18c4ba3166e5bdbe133a4d8594dcace93331bc638da39e5a6760e573bdb1b29af275a3f0a7897b14e444fd07067f8940bb4c6d639f8'
