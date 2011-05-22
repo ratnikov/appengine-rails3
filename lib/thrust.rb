e9aa0be24242c493038860bc6c3a5ade37a24f24
Dmitry Ratnikov <ratnikov@gmail.com>
2011-05-22 13:18:25 -0400

    Made to require all the jars in the vendor/appengine-java-sdk directory automatically

--- lib/thrust.rb
+++ lib/thrust.rb
@@ -1,6 +1,8 @@
 require 'java'
 
-require 'appengine-sdk/appengine-api.jar'
+Dir.glob(File.join(Rails.root, 'vendor', 'appengine-java-sdk', '*.jar')).each do |jar|
+  require jar
+end
 
 require 'thrust/authentication'
 require 'thrust/dev_server'
