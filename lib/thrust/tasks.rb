57787902c8ea74f791659b9093b7323adcaedcda
Dmitry Ratnikov <ratnikov@gmail.com>
2011-05-29 18:29:21 -0400

    Added a task to pull sdk.

--- lib/thrust/tasks.rb
+++ lib/thrust/tasks.rb
@@ -1,5 +1,7 @@
 require 'rake'
 
+SDK_LOCATION = 'http://googleappengine.googlecode.com/files/appengine-java-sdk-1.5.0.1.zip'
+
 config = Warbler::Config.new do |config|
   config.jar_name = 'package'
   config.features << 'gemjar'
@@ -9,12 +11,33 @@ end
 
 Warbler::Task.new 'war', config
 
+namespace :thrust do
+  task 'install-sdk' do
+    unless File.exists?('sdk')
+      zip_location = "tmp/%s" % File.basename(SDK_LOCATION)
+      if File.exists? zip_location
+        puts "Using existing zip package..."
+      else
+        puts "Downloading appengine sdk from #{SDK_LOCATION}..."
+        system "curl #{SDK_LOCATION} -o #{zip_location}"
+      end
+
+      puts "Unpacking..."
+      system "unzip -qq -fo #{zip_location} -d tmp"
+
+      system "rm -rf sdk && ln -s tmp/#{File.basename(zip_location, '.zip')} sdk"
+      puts "Done!"
+    else
+      puts "Seems like sdk is already installed. Nothing to do!"
+    end
+  end
+end
 
 namespace :war do
   desc "Creates and unpacks the application war into war/"
   task :unpack => [ 'war' ]do
     system "rm -rf war/; mkdir war/"
 
-    system "cd war && jar xf ../package.war && cd .."
+    Dir.chdir("war") { system "jar xf ../package.war" }
   end
 end
