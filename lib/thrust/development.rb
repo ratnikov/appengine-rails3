fe0042d426c4a63b197e97c5511d69a10f8213c3
Dmitry Ratnikov <ratnikov@gmail.com>
2011-05-30 22:20:01 -0400

    Moved development jars to vendor/appengine*/development

--- lib/thrust/development.rb
+++ lib/thrust/development.rb
@@ -1,2 +1,7 @@
 require 'thrust'
+
+Dir.glob(File.expand_path("../../../vendor/appengine*/development/**.jar", __FILE__)).each do |dev_jar|
+  require dev_jar
+end
+
 require 'thrust/development/middleware'
