54fa5f254424487b7a035013bd9624b1581447d0
Dmitry Ratnikov <ratnikov@gmail.com>
2011-06-05 21:25:02 -0400

    Made sure to run only on examples that don't have :thrust => false

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,7 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
   # enable thrust environment except for :thrust => false examples
-  config.around(:thrust => proc { |val| val }) { |example| Thrust::Development.engaged { example.run } }
+  config.around do |example| 
+    Thrust::Development.engaged { example.run } unless example.metadata[:thrust] == false
+  end
 end
