94c7f56d78d5b4a8a72b11940ff7308d5070f5a4
Dmitry Ratnikov <ratnikov@gmail.com>
2011-06-14 03:45:38 -0400

    Added require_admin before filter for destroy action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include Thrust::ControllerExtensions
   protect_from_forgery
+
+  private
+
+  def require_admin
+    render :text => 'Not authorized', :status => :forbidden unless logged_in? && admin?
+  end
 end
