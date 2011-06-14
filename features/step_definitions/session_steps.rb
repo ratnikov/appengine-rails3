9eca4c63d7c94116541113e2b00f6ce6e9dff055
Dmitry Ratnikov <ratnikov@gmail.com>
2011-06-14 03:00:53 -0400

    Implemented admin logging in step.

--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -1,9 +1,11 @@
-Given /^I am logged in as "(.*)"$/ do |email|
+Given /^I am logged in as "(.*)"( admin)?$/ do |email, admin|
   visit '/'
 
   click_link 'login'
 
   fill_in 'email', :with => email
+  check 'admin' unless admin.blank?
+
   click_button 'Log in!'
 end
 
